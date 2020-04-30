031828f74eef0b1471d77aba977100ebeb5c7b6c
1995zmj <minjiangzhao@icloud.com>
2020-04-30 20:32:35 +0800

    更新

---
+++ Assets/MyCoroutine.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class MyCoroutine : MonoBehaviour
+{
+    private int i;
+    void Start()
+    {
+        i = 0;
+        StartCoroutine(Thread1());
+        var tempGameObject = new GameObject("zmj");
+        UnityEngine.Object.DontDestroyOnLoad(tempGameObject);
+        var gameobjct = GameObject.Find("zmj");
+        gameobjct.SetActive(false);
+    }
+
+    void Update()
+    {
+        
+    }
+
+    IEnumerator Thread1()
+    {
+        while(true){
+            Debug.Log("i="+ i);
+            i++;
+            if(i >= 10)
+            {
+                
+                break;
+            }
+            //等待一面
+            yield return StartCoroutine(Thread2());
+        }
+    }
+
+    IEnumerator Thread2()
+    {
+        Debug.Log("show idle");
+        yield return null;
+    }
+}
